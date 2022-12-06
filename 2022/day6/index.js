5d6e54ab2e552db413854628b52aff58db485f42
Sai Shanmukh <msshanmukh@gmail.com>
2022-12-06 18:32:07 +0530

    day6 alt solution

--- 2022/day6/index.js
+++ 2022/day6/index.js
@@ -1,22 +1,41 @@
 let fs = require('fs');
 
-let d = fs.readFileSync('./input.txt', 'utf-8').split('');
+let input = fs.readFileSync('./input.txt', 'utf-8').split('');
+let input2 = fs.readFileSync('./input.txt', 'utf-8');
 
 /*
 bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
 nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
 nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
 zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
-
 */
 
 function solve(k) {
 	return (
-		d
-			.map((_, i) => d.slice(i, i + k))
+		input
+			.map((_, i) => input.slice(i, i + k))
 			.findIndex((a) => new Set(a).size == a.length) + k
 	);
 }
 
 console.log(solve(4));
 console.log(solve(14));
+
+// long method
+function solve2(packetSize) {
+	for (let i = 0; i < input2.length - packetSize; i++) {
+		let tempArr = input2.substring(i, i + packetSize).slice('');
+		let uniqueSet = new Set(tempArr);
+		if (uniqueSet.size == packetSize) {
+			console.log(i + packetSize);
+			break;
+		} else {
+			uniqueSet.clear();
+		}
+	}
+}
+
+// solution 1
+solve2(4);
+// solution 2
+solve2(14);
